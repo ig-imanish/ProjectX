eec3d1f547a11a0615fda51789bb5e67f61f8ef1
Manish Kumar <153966717+ig-imanish@users.noreply.github.com>
2024-03-12 19:23:58 +0530

    Delete projectx directory

--- projectx/src/main/java/com/projectx/projectx/dao/PostRepo.java
+++
@@ -1,8 +0,0 @@
-package com.projectx.projectx.dao;
-
-
-import org.springframework.data.repository.CrudRepository;
-import com.projectx.projectx.entities.Post;
-public interface PostRepo extends CrudRepository<Post, Long>{
-    
-}
